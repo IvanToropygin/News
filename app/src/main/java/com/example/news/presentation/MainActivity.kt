af82c5c2bac87f5081488f007f194b4f05f4ffd3
Ivan Toropygin <itori86@mail.ru>
2026-01-31 20:45:39 +0300

    add SettingsScreen

--- app/src/main/java/com/example/news/presentation/MainActivity.kt
+++ app/src/main/java/com/example/news/presentation/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.news.presentation
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import com.example.news.presentation.screen.subscriptions.SubscriptionsScreen
+import com.example.news.presentation.screen.settings.SettingsScreen
 import com.example.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,16 +16,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
-                val prmLauncher = rememberLauncherForActivityResult(
-                    contract = ActivityResultContracts.RequestPermission(),
-                    onResult = {}
-                )
-                SubscriptionsScreen(
-                    onNavigateToSettings = {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                            prmLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-                        }
-                    }
+                SettingsScreen(
+                    onBackClick = { }
                 )
             }
         }
